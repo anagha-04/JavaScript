8803d317922c68c6b4f06e0278175addfba012b1
anagha-04 <anaghanilkumar04@gmail.com>
2025-12-19 10:42:52 +0530

    basic

---
+++ sample.js
@@ -0,0 +1,26 @@
+// a=10 name ="hello"
+
+// variable keyword
+
+//var let const
+
+var name ="python"
+
+name ="django"
+
+console.log(name)    // value assigined in a variable can be changed
+
+
+let subject ="datascience"  // iteration 
+
+subject = "django"
+
+console.log(subject)  // value assigined in a variable can be changed
+
+
+const age = 26   //fun
+
+age = 30
+
+console.log(age)
+
