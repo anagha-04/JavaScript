bdb218fb3f74e74c28f25b8252049b5c43f2d354
anagha-04 <anaghanilkumar04@gmail.com>
2025-12-23 09:28:53 +0530

    problems

---
+++ largestAmong.js
@@ -0,0 +1,20 @@
+//  check the largest among three number
+
+
+var num1 = 4
+
+var num2 = 6
+
+var num3 = 9
+
+if(num1 > num2 && num1 > num3)
+    {
+    console.log(num1,"is largest number")
+}
+else if(num2 > num1 && num2 > num3)
+    {
+    console.log(num2, "is largest number")
+}
+else{
+    console.log(num3,"is largest number")
+}
