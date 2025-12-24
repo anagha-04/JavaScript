048e7ee3685f0e03b10537f6ee390062a7577c2c
anagha-04 <anaghanilkumar04@gmail.com>
2025-12-24 09:38:26 +0530

    looping

---
+++ looping.js
@@ -0,0 +1,16 @@
+// what is looping 
+
+// which allows a block of code to be executed repeatedly
+
+
+// Type of looping 
+
+// for loop (it is a quantity based looping)
+
+// syntax
+
+// for(initialization;condition;increment/decrement){
+    //    code to be executed repeatedly 
+    // }
+
+// While loop(condition based)
