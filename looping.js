74ce0d1e4c742a2220211c65d3ccbd5bd423d025
anagha-04 <anaghanilkumar04@gmail.com>
2025-12-24 09:54:45 +0530

    lp

--- looping.js
+++ looping.js
@@ -13,4 +13,26 @@
     //    code to be executed repeatedly 
     // }
 
-// While loop(condition based)
+
+
+for(i=0; i<=5;i++){
+    console.log(i)
+}
+
+// iterating over  fixed range of number
+
+// print the number from 10 to 1
+
+for (i=10;i>=1;i--){
+    console.log(i)
+}
+
+// print even numbers between 1 and 20
+
+for(i=0;i<=20;i++){
+
+    if(i%2==0){
+        
+        console.log(i)
+    }
+}
