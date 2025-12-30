e45dac45d292a172cbf55a1c90043e867d6d3d0a
anagha-04 <anaghanilkumar04@gmail.com>
2025-12-30 10:26:15 +0530

    collections

---
+++ collections.js
@@ -0,0 +1,13 @@
+
+// collections
+// ===========
+
+
+// to assign more than one value (in different dataype) in a single variable
+
+
+//  array  : ordered
+
+let elements = [100,"hello",true]
+
+console.log(typeof(elements))
