d06040a743896c0e74734148b1468ac779601548
anagha-04 <anaghanilkumar04@gmail.com>
2025-12-31 09:30:00 +0530

    commit

--- collections.js
+++ collections.js
@@ -6,8 +6,34 @@
 // to assign more than one value (in different dataype) in a single variable
 
 
-//  array  : ordered
+//  array  : ordered,indexed,allow duplications
 
 let elements = [100,"hello",true]
 
-console.log(typeof(elements))
+console.log(elements[1])   //return the item in index position
+
+elements.push(false)
+
+console.log(elements)
+
+elements.pop(elements)
+
+console.log(elements.toReversed())
+
+
+// find the even numbers from a given range and display them in a array
+
+
+let start = 3
+
+let stop = 20
+
+let evenNums = []
+
+for(let i = start; i<=stop; i++){
+
+    if(i %2 == 0){
+        evenNums.push(i)
+    }
+}
+console.log(evenNums)
