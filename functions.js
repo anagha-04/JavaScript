11987153e391ba728148765a3def6c93438cfb84
anagha-04 <anaghanilkumar04@gmail.com>
2026-01-06 09:30:26 +0530

    commit

---
+++ functions.js
@@ -0,0 +1,26 @@
+let num1 = 10
+
+let num2 = 20
+
+console.log(num1+num2)
+
+//  function
+
+// export import
+// reusable
+// dynamic values 
+
+// function  functionname(param1,param2){  ----> function header
+
+//       function body         ----> fn body
+
+// }
+// functioname()         ----->function caller
+
+
+function greet(){
+
+    console.log("hello everyone")
+}
+
+greet()
