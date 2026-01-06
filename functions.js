d2287b073197cbf071a964b5f1b9aca9f22ce675
anagha-04 <anaghanilkumar04@gmail.com>
2026-01-06 09:43:07 +0530

    fn

--- functions.js
+++ functions.js
@@ -24,3 +24,20 @@ function greet(){
 }
 
 greet()
+
+//  function wit arameter
+// parameter is a variable which accepts a value when the function is called
+
+function addNum(num1,num2){
+
+    sum = num1+num2
+
+    console.log(sum)
+
+}
+
+addNum(num1 = 10, num2 = 20)  // actual values for parameters passed during function call 
+addNum(num1 = 3, num2 = 2)
+
+
+//  function with return value
