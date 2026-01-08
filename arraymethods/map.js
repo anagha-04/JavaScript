504d8c3b270dda68deb1d29c63f5a7430ae0063d
anagha-04 <anaghanilkumar04@gmail.com>
2026-01-08 09:26:21 +0530

    array

---
+++ arraymethods/map.js
@@ -0,0 +1,12 @@
+//  array >> map,filter,reduce
+
+
+// we can apply a specific function to each  element in array without using loops
+// [1,2,3,4]   [1,4,9,16]
+
+// syntax
+
+// array.map(item,index) =>{
+//   return new value
+//   }
+
