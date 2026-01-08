259dfab744a0999b4bec9ae4d7a75bbbad158e36
anagha-04 <anaghanilkumar04@gmail.com>
2026-01-08 09:34:28 +0530

    array

--- arraymethods/map.js
+++ arraymethods/map.js
@@ -10,3 +10,19 @@
 //   return new value
 //   }
 
+let numbers = [2,3,4,5,6]
+
+console.log(numbers.map(num => num**2))
+
+
+// find the square root of each element in the array
+// numbers = [4,9,16,25]
+
+let num =[4,9,16,25]
+console.log(num.map(n => n**2))
+
+// find the cube of each element in the array
+// numbers =[1,2,3,4]
+
+let nums =[1,2,3,4]
+console.log(nums.map(nu => nu**3))
