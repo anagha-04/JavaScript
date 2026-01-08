aed6ca7cdbd3f6b247f7497aa705321e05b76bd7
anagha-04 <anaghanilkumar04@gmail.com>
2026-01-08 10:23:46 +0530

    asyn

---
+++ Asynchronouns Js/sample.js
@@ -0,0 +1,17 @@
+// synchronous ==>>> Asynchronous
+
+// Asynchronous Javascript
+// ========================
+
+
+// API fetch
+// database queries
+// validation
+
+console.log("hello")
+
+setTimeout(()=>{
+    console.log("js")
+},2000)
+
+console.log("django")
