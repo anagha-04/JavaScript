e1b5f32d884499517beede2275111d554afc73f0
anagha-04 <anaghanilkumar04@gmail.com>
2026-01-12 10:04:15 +0530

    dom

---
+++ arraymethods/reviews.js
@@ -0,0 +1,13 @@
+
+
+
+// DOM
+// ====
+
+// the object model of html(js engine) which is used to connect js and html
+
+// when a html is rendered on any browser it converts the page into object called document so js can understand it 
+
+// it can make changes dynamically in the html(not in main script)
+
+// DOM helps to make dynamic changes when fetching data from api and update it in html
