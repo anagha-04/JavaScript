14c71f621fca1d6cb7d55a86fe8647318fd5dc60
anagha-04 <anaghanilkumar04@gmail.com>
2026-01-15 10:03:12 +0530

    gg

--- Guess/script.js
+++ Guess/script.js
@@ -7,11 +7,36 @@ function check(){
     let guess = document.querySelector("#number").value
 
     if (guess == secretNum){
-
         document.querySelector("#secret").innerText = guess
+        
+      if(score >highest){
+        highest = score
+        document.querySelector("#highest").innerText=highest
+      }
+
     }
     else if (guess == ""){
         document.querySelector("#secret").innerText ="No Number"
     }
+    if (guess<secretNum){
+
+        if(score>1){
+            score--
+        }
+    }
 
 }
+
+if (guess == secretNum) {
+        document.querySelector("#secret").innerHTML = guess
+        document.querySelector("#msg").innerHTML = "Correct Number!";
+    } else if (guess == "") {
+        document.querySelector("#secret").innerHTML = "no number"
+    }
+
+    else {
+        document.querySelector("#msg").innerHTML =
+            guess > secretNum ? "Too High!" : "Too Low!";
+        score--;
+        document.querySelector("#score").innerHTML = score;
+    }
