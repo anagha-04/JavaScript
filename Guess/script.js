02b2a1c0da609ca8041fee7ae396e798f2339170
anagha-04 <anaghanilkumar04@gmail.com>
2026-01-15 09:38:03 +0530

    gs

--- Guess/script.js
+++ Guess/script.js
@@ -1,3 +1,17 @@
 let secretNum = Math.floor(Math.random()*20) + 1
 let score = 20
 let highest = 0
+
+function check(){
+
+    let guess = document.querySelector("#number").value
+
+    if (guess == secretNum){
+
+        document.querySelector("#secret").innerText = guess
+    }
+    else if (guess == ""){
+        document.querySelector("#secret").innerText ="No Number"
+    }
+
+}
